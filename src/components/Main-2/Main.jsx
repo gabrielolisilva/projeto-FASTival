6793115390abb836ed3d8363b4d807f7a6045383
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-10-31 13:58:36 -0300

    Components addition

--- src/components/Main-2/Main.jsx
+++ src/components/Main-2/Main.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import "./Main.css";
 
+import imageColdplay from "../../assets/coldplay.png";
+
 const Main = () => {
   return (
     <div className="main2">
       <h2>Próximos shows !</h2>
       <div className="quadro">
         <div className="elementoEsquerda">
-          <p>Elemento Esquerda</p>
+          <h2>Show do Coldplay</h2>
+          <h5>SAB, 24 de Dezembro</h5>
+          <p>Allianz Parque - São Paulo, SP</p>
+          <button>Saiba mais</button>
         </div>
         <div className="elementoDireita">
-          <p>Elemento Direita</p>
+          <img src={imageColdplay} alt="coldplayImage" />
         </div>
+      </div>
+      <div className="links">
+        <span className="active"></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   );
